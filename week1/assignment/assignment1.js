77f40e906a999a5fcad6e921ee38f3591760df51
JUNW0O <wnsdn180700@naver.com>
2024-03-23 21:51:18 +0900

    finished task 3

--- week1/assignment/assignment1.js
+++ week1/assignment/assignment1.js
@@ -4,38 +4,67 @@ num = Number(num);
 let auto = prompt("자동 클릭 횟수를 정할 수 있습니다.");
 auto = Number(auto);
 
-const count_dom = document.getElementById("count");
-const egg_dom = document.getElementById("egg");
+const count1_dom = document.getElementById("count1");
+const count2_dom = document.getElementById("count2");
+const egg1_dom = document.getElementById("egg1");
+const egg2_dom = document.getElementById("egg2");
 
-count_dom.innerHTML = num;
+count1_dom.innerHTML = num;
+count2_dom.innerHTML = num;
 
-let maxnum = num;
+let num1 = num;
+let num2 = num;
 
-const hitEgg = () => {
-  count_dom.innerHTML = --num;
+const hitEgg1 = () => {
+  count1_dom.innerHTML = --num1;
 
-  if (num < (maxnum * 1) / 7) {
-    count_dom.style.color = "purple";
-  } else if (num < (maxnum * 2) / 7) {
-    count_dom.style.color = "darkblue";
-  } else if (num < (maxnum * 3) / 7) {
-    count_dom.style.color = "blue";
-  } else if (num < (maxnum * 4) / 7) {
-    count_dom.style.color = "green";
-  } else if (num < (maxnum * 5) / 7) {
-    count_dom.style.color = "yellow";
-  } else if (num < (maxnum * 6) / 7) {
-    count_dom.style.color = "orange";
+  if (num1 < (num * 1) / 7) {
+    count1_dom.style.color = "purple";
+  } else if (num1 < (num * 2) / 7) {
+    count1_dom.style.color = "darkblue";
+  } else if (num1 < (num * 3) / 7) {
+    count1_dom.style.color = "blue";
+  } else if (num1 < (num * 4) / 7) {
+    count1_dom.style.color = "green";
+  } else if (num1 < (num * 5) / 7) {
+    count1_dom.style.color = "yellow";
+  } else if (num1 < (num * 6) / 7) {
+    count1_dom.style.color = "orange";
   }
 
-  if (num == 0) {
+  if (num1 == 0) {
     alert("축하합니다!");
-    egg_dom.removeEventListener("click", hitEgg);
+    egg1_dom.removeEventListener("click", hitEgg1);
   }
 };
 
-egg_dom.addEventListener("click", hitEgg);
+const hitEgg2 = () => {
+  count2_dom.innerHTML = --num2;
+
+  if (num2 < (num * 1) / 7) {
+    count2_dom.style.color = "purple";
+  } else if (num2 < (num * 2) / 7) {
+    count2_dom.style.color = "darkblue";
+  } else if (num2 < (num * 3) / 7) {
+    count2_dom.style.color = "blue";
+  } else if (num2 < (num * 4) / 7) {
+    count2_dom.style.color = "green";
+  } else if (num2 < (num * 5) / 7) {
+    count2_dom.style.color = "yellow";
+  } else if (num2 < (num * 6) / 7) {
+    count2_dom.style.color = "orange";
+  }
+
+  if (num2 == 0) {
+    alert("축하합니다!");
+    egg2_dom.removeEventListener("click", hitEgg2);
+  }
+};
+
+egg1_dom.addEventListener("click", hitEgg1);
+egg2_dom.addEventListener("click", hitEgg2);
 
 for (let i = 0; i < auto; i++) {
-  egg_dom.click();
+  egg1_dom.click();
+  egg2_dom.click();
 }
