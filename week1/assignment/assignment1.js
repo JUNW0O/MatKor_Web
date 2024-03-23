1e46d72258227968cccc32d28716daecee20a6b5
JUNW0O <wnsdn180700@naver.com>
2024-03-23 21:31:45 +0900

    finished task 2

--- week1/assignment/assignment1.js
+++ week1/assignment/assignment1.js
@@ -9,8 +9,25 @@ const egg_dom = document.getElementById("egg");
 
 count_dom.innerHTML = num;
 
+let maxnum = num;
+
 const hitEgg = () => {
   count_dom.innerHTML = --num;
+
+  if (num < (maxnum * 1) / 7) {
+    count_dom.style.color = "purple";
+  } else if (num < (maxnum * 2) / 7) {
+    count_dom.style.color = "darkblue";
+  } else if (num < (maxnum * 3) / 7) {
+    count_dom.style.color = "blue";
+  } else if (num < (maxnum * 4) / 7) {
+    count_dom.style.color = "green";
+  } else if (num < (maxnum * 5) / 7) {
+    count_dom.style.color = "yellow";
+  } else if (num < (maxnum * 6) / 7) {
+    count_dom.style.color = "orange";
+  }
+
   if (num == 0) {
     alert("축하합니다!");
     egg_dom.removeEventListener("click", hitEgg);
