875fdd97f7e6c05fd503ffa81a21df0e617d9eb0
JUNW0O <wnsdn180700@naver.com>
2024-03-23 21:08:09 +0900

    added basic elements

--- week1/assignment/assignment1.js
+++ week1/assignment/assignment1.js
@@ -1,4 +1,17 @@
-let num = prompt("도전할 숫자를 정하세요!");
+let num = prompt("도전할 숫자를 정해주세요!");
 num = Number(num);
 
 const count_dom = document.getElementById("count");
+const egg_dom = document.getElementById("egg");
+
+count_dom.innerHTML = num;
+
+const hitEgg = () => {
+  count_dom.innerHTML = --num;
+  if (num == 0) {
+    alert("축하합니다!");
+    egg_dom.removeEventListener("click", hitEgg);
+  }
+};
+
+egg_dom.addEventListener("click", hitEgg);
