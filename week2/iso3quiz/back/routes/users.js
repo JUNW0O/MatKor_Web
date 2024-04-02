1685c6b02e40fa469d615eaf83db1384736826f2
JUNW0O <wnsdn180700@naver.com>
2024-04-02 10:07:35 +0900

    quiz 생성 기능 추가

--- week2/iso3quiz/back/routes/users.js
+++ week2/iso3quiz/back/routes/users.js
@@ -1,9 +1,23 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
+}
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.get("/quiz", function (req, res, next) {
+  const countries = require("../../../data/countries.json");
+  const index = getRandomInt(0, countries.length + 1);
+  const quiz = countries[index];
+  console.log(quiz);
+  res.status(200).json({ country: quiz.name });
 });
 
 module.exports = router;
--- week2/iso3quiz/front/src/App.js
+++ week2/iso3quiz/front/src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [country, setCountry] = useState("");
+
+  const getQuiz = () => {
+    axios
+      .get("api/users/quiz")
+      .then((res) => {
+        setCountry(res.data.country);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>ISO3 Quiz</h1>
+        <p>주어진 나라 이름에 맞는 ISO 국가 코드를 맞혀보세요!</p>
+        <p>정답은 항상 세 글자 알파벳입니다.</p>
+        <button onClick={getQuiz}>새로운 퀴즈 받기</button>
+        {country === "" ? null : (
+          <>
+            <h4>문제: {country}</h4>
+            <form>
+              정답: <input type="text" name="answer"></input>
+              <button>제출</button>
+            </form>
+          </>
+        )}
       </header>
     </div>
   );
